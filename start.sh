a6644ed55ca2c7825aa84b02bb913218d5e2aeaa
lucalocolocoloco <edijarqo@gmail.com>
2024-12-24 18:41:46 -0500

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Ubah direktori ke /home/coder/project
+cd /home/
+
+# Membuat pengguna baru 'fbbot' tanpa password dan menambahkannya ke grup 'sudo'
+sudo adduser --disabled-password --gecos "" xb
+sudo usermod -aG sudo xb
+
+# Membuat virtual environment Python
+python3 -m venv v 
+source v/bin/activate 
+
+# Menginstal paket Python dan Node.js
+pip install pyautogui 
+
+
+# Mulai code-server tanpa otentikasi pada port 8080
+echo "Starting Tmate.."
+tmate -F . &
+
+# Tunggu selama 10 detik agar code-server siap
+sleep 4
+
+# Mencegah script keluar dan container berhenti
+echo "tmate is running. Keeping the container alive..."
+tail -f /dev/null
